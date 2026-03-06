089b2d6cd1847758655b0633513f9b5e28e061d3
Caleb Hansen <calebhansen1@wsu.edu>
2026-03-05 19:36:26 -0800

    Initial MPI setup

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <mpi.h>
+#include <assert.h>
 
-int main(void) {
+int main(int argc, char* argv[]) {
     printf("Hello World\n");
+    
+    MPI_Init(&argc, &argv);
+
+    const int n = 16;
+    const int G = 2;
+
+    int rank, p;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
+    
+    assert(n > p && n % p == 0);
+
     return 0;
 }
